1d910dbf83a84e82b204e36ab4942c75f72de226
Unknown <hmtai6@student.cityu.edu.hk>
2017-05-13 12:44:31 +0800

    update parser

--- voc_parser.py
+++ voc_parser.py
@@ -26,7 +26,7 @@ def get_data(input_path):
 		annot_path = os.path.join(data_path, 'Annotations')
 		imgs_path = os.path.join(data_path, 'JPEGImages')
 		imgsets_path_trainval = os.path.join(data_path, 'ImageSets','Main','trainval.txt')
-		imgsets_path_test = os.path.join(data_path, 'ImageSets','Main','test.txt')
+#		imgsets_path_test = os.path.join(data_path, 'ImageSets','Main','test.txt')
 
 		trainval_files = []
 		test_files = []
@@ -34,9 +34,9 @@ def get_data(input_path):
 			with open(imgsets_path_trainval) as f:
 				for line in f:
 					trainval_files.append(line.strip() + '.jpg')
-			with open(imgsets_path_test) as f:
-				for line in f:
-					test_files.append(line.strip() + '.jpg')
+#			with open(imgsets_path_test) as f:
+#				for line in f:
+#					test_files.append(line.strip() + '.jpg')
 		except Exception as e:
 			print(e)
 
@@ -74,17 +74,21 @@ def get_data(input_path):
 						classes_count[class_name] += 1
 
 					if class_name not in class_mappingNameToId:
-						class_mappingNameToId[class_name] = len(class_mappingNameToId)
-						class_mappingIdToName[len(class_mappingNameToId)] = class_name
+						class_mappingNameToId[class_name] = len(class_mappingNameToId) 
+						class_mappingIdToName[len(class_mappingNameToId)-1] = class_name
 
 					obj_bbox = element_obj.find('bndbox')
 					x1 = int(round(float(obj_bbox.find('xmin').text)))
 					y1 = int(round(float(obj_bbox.find('ymin').text)))
 					x2 = int(round(float(obj_bbox.find('xmax').text)))
 					y2 = int(round(float(obj_bbox.find('ymax').text)))
+					w = x2-x1
+					h = y2-y1
+					x = int(round(x1+w/2))
+					y = int(round(y1+h/2))
 					difficulty = int(element_obj.find('difficult').text) == 1
 					annotation_data['bboxes'].append(
-						{'class': class_name, 'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2, 'difficult': difficulty})
+						{'class': class_name, 'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2, 'difficult': difficulty, 'x':x, 'y':y, 'w':w, 'h':h})
 				all_imgs.append(annotation_data)
 
 				if visualise:
@@ -95,26 +99,26 @@ def get_data(input_path):
 					cv2.imshow('img', img)
 					cv2.waitKey(0)
 
-				# make if no bg in the className make bg class
-				if 'bg' not in classes_count:
-					classes_count['bg'] = 0
-					class_mappingNameToId['bg'] = len(class_mappingNameToId)
-					class_mappingIdToName[len(class_mappingNameToId)] = 'bg'
-				else:
-				# if there are a bg class make it last index
-					tempId = class_mappingNameToId['bg']
-					class_mappingNameToId['bg'] = len(class_mappingNameToId)-1
-					lastName = class_mappingIdToName[len(class_mappingNameToId)-1]
-					class_mappingNameToId[lastName] = tempId
-					class_mappingIdToName[tempId] = lastName
-					class_mappingIdToName[len(class_mappingNameToId)] = 'bg'
-
-
 
 			except Exception as e:
 				print(e)
 				continue
 
+		# make if no bg in the className make bg class
+		if 'bg' not in classes_count:
+			print("bg not in class")
+			classes_count['bg'] = 0
+			class_mappingNameToId['bg'] = len(class_mappingNameToId)
+			class_mappingIdToName[len(class_mappingNameToId)] = 'bg'
+		else:
+			print("bg in class")
+		# if there are a bg class make it last index
+			tempId = class_mappingNameToId['bg']
+			class_mappingNameToId['bg'] = len(class_mappingNameToId)-1
+			lastName = class_mappingIdToName[len(class_mappingNameToId)-1]
+			class_mappingNameToId[lastName] = tempId
+			class_mappingIdToName[tempId] = lastName
+			class_mappingIdToName[len(class_mappingNameToId)] = 'bg'
 	return all_imgs, classes_count, class_mappingNameToId, class_mappingIdToName
 	
 all_imgs, classes_count, class_mappingNameToId, class_mappingIdToName = get_data("./data")
